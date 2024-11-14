5015516983479acd022a0cd3319acba4ae254429
Sebastian <sebastian.franz@tum.de>
2024-11-14 15:45:03 +0100

    Improving biotrainer log file reading with last read position
    Only reads and sends new lines in the log file
    
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- biocentral_server/biocentral_server/prediction_models/biotrainer_process.py
+++ biocentral_server/biocentral_server/prediction_models/biotrainer_process.py
@@ -16,6 +16,7 @@ class BiotrainerProcess(TaskInterface):
     def __init__(self, config_path: Path, log_path: Path):
         self.config_path = config_path
         self.log_path = log_path
+        self._last_read_position_in_log_file = 0
 
     async def start(self):
         self.process = torch_mp.Process(target=headless_main, args=(str(self.config_path),))
@@ -31,5 +32,8 @@ class BiotrainerProcess(TaskInterface):
         result = {"log_file": "", "status": self.get_task_status().name}
         if os.path.exists(self.log_path):
             with open(self.log_path, "r") as log_file:
-                result["log_file"] = log_file.read()
+                log_file.seek(self._last_read_position_in_log_file)
+                new_content = log_file.read()
+                self._last_read_position_in_log_file = log_file.tell()
+                result["log_file"] = new_content
         return result
