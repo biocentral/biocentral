a6090d9db9757cad2f53acf579667f48cf191723
Sebastian <sebastian.franz@tum.de>
2024-12-17 09:39:37 +0100

    Adding recommended metrics to plm leaderboard
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- lib/plugins/plm_eval/model/plm_leaderboard.dart
+++ lib/plugins/plm_eval/model/plm_leaderboard.dart
@@ -4,7 +4,6 @@ import 'package:biocentral/plugins/prediction_models/data/prediction_models_serv
 import 'package:biocentral/plugins/prediction_models/model/prediction_model.dart';
 import 'package:biocentral/sdk/biocentral_sdk.dart';
 import 'package:biocentral/sdk/data/biocentral_task_dto.dart';
-import 'package:collection/collection.dart';
 import 'package:fpdart/fpdart.dart';
 
 /// Helper class to combine PredictionModel with dataset-specific information
@@ -25,17 +24,28 @@ class PLMLeaderboard {
 
   final List<(String, double)> ranking;
 
-  const PLMLeaderboard._({required this.entries, required this.ranking});
+  final Map<String, String> recommendedMetrics;
 
-  const PLMLeaderboard.empty() : entries = const [], ranking = const [];
+  const PLMLeaderboard._({required this.entries, required this.ranking, required this.recommendedMetrics});
+
+  const PLMLeaderboard.empty()
+      : entries = const [],
+        ranking = const [],
+        recommendedMetrics = const {};
 
   static Either<BiocentralParsingException, PLMLeaderboard> fromDTO(BiocentralDTO fullDTO) {
     final List<PLMLeaderboardEntry> parsedEntries = [];
 
+    final recommendedMetrics = fullDTO.recommendedMetrics;
+
+    if (recommendedMetrics.isEmpty) {
+      // TODO Throw exception
+    }
+
     final sortedRanking = fullDTO.ranking.entries.toList()..sort((e1, e2) => e1.value.compareTo(e2.value));
     final sortedRankingTuples = List<(String, double)>.from(sortedRanking.map((entry) => (entry.key, entry.value)));
 
-    if(sortedRankingTuples.isEmpty) {
+    if (sortedRankingTuples.isEmpty) {
       // TODO Throw exception
     }
 
@@ -91,7 +101,12 @@ class PLMLeaderboard {
         PLMLeaderboardEntry(model: modelWithResults, benchmarkDataset: benchmarkDataset, trainingDate: trainingDate),
       );
     }
-    return right(PLMLeaderboard._(entries: parsedEntries, ranking: sortedRankingTuples));
+    return right(
+        PLMLeaderboard._(entries: parsedEntries, ranking: sortedRankingTuples, recommendedMetrics: recommendedMetrics));
+  }
+
+  String getRecommendedMetricByDataset(BenchmarkDataset dataset) {
+    return recommendedMetrics[dataset.datasetName] ?? 'loss';
   }
 
   // Get all unique dataset-split combinations
@@ -102,79 +117,14 @@ class PLMLeaderboard {
     return entries.where((e) => e.benchmarkDataset == benchmark).toList();
   }
 
-  // Calculate rankings for each benchmark dataset
-  Map<BenchmarkDataset, Map<String, int>> calculateBenchmarkRankings(String metricName) {
-    final rankings = <BenchmarkDataset, Map<String, int>>{};
-    final bool higherIsBetter = !BiocentralMLMetric.isAscending(metricName);
-
-    // Calculate rankings for each benchmark dataset
-    for (final benchmark in benchmarkDatasets) {
-      final benchmarkEntries = getEntriesForBenchmark(benchmark);
-      final embedderScores = <String, double>{};
-
-      // Get scores for each model
-      for (final entry in benchmarkEntries) {
-        final modelName = entry.model.embedderName;
-        if (modelName == null) continue;
-
-        final metric =
-            entry.model.biotrainerTrainingResult?.testSetMetrics.firstWhereOrNull((m) => m.name == metricName)?.value;
-
-        if (metric != null) {
-          embedderScores[modelName] = metric;
-        }
-      }
-
-      // Sort models by score
-      final sortedEmbedders = embedderScores.keys.toList()
-        ..sort((a, b) {
-          final comparison = embedderScores[b]!.compareTo(embedderScores[a]!);
-          return higherIsBetter ? comparison : -comparison;
-        });
-
-      // Assign rankings
-      rankings[benchmark] = Map.fromIterables(
-        sortedEmbedders,
-        List.generate(sortedEmbedders.length, (index) => index + 1).reversed,
-      );
-    }
-
-    return rankings;
-  }
-
-  // Calculate global ranking across all benchmarks
-  List<(String, int)> calculateGlobalRanking({
-    String metricName = 'loss',
-  }) {
-    final benchmarkRankings = calculateBenchmarkRankings(metricName);
-
-    // Collect all rankings for each model
-    final allRankings = <String, List<int>>{};
-
-    for (final rankingEntry in benchmarkRankings.entries) {
-      for (final modelRanking in rankingEntry.value.entries) {
-        allRankings.putIfAbsent(modelRanking.key, () => []).add(modelRanking.value);
-      }
-    }
-
-    final globalScores = allRankings
-        .map((model, rankings) => MapEntry(model, rankings.reduce((a, b) => a + b)) // / rankings.length for average
-            );
-
-    // Sort by average ranking
-    final sortedGlobalScores = globalScores.entries.toList()..sort((a, b) => a.value.compareTo(b.value));
-
-    return sortedGlobalScores.map((e) => (e.key, e.value)).toList().reversed.toList();
-  }
-
   Map<String, Set<BiocentralMLMetric>> getMetricsForBenchmark(BenchmarkDataset benchmark) {
     final relevantEntries = getEntriesForBenchmark(benchmark);
 
     return Map.fromEntries(
       relevantEntries.map((entry) => MapEntry(
-        entry.model.embedderName ?? 'Unknown',
-        entry.model.biotrainerTrainingResult?.testSetMetrics ?? {},
-      )),
+            entry.model.embedderName ?? 'Unknown',
+            entry.model.biotrainerTrainingResult?.testSetMetrics ?? {},
+          )),
     );
   }
 }
