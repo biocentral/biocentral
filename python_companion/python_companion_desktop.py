ad62358de55acbffdf2c04852ce7e70f0ad5c869
Sebastian <sebastian.franz@tum.de>
2025-02-13 16:05:54 +0100

    Adding serious_python companion to build instructions and workflow
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- python_companion/python_companion_desktop.py
+++ python_companion/python_companion_desktop.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     run_server()
 
 # Compile via
-# dart run serious_python:main package --asset assets/python_companion.zip python_companion/ -p Linux --requirements -r,python_companion/requirements.txt
+# dart run serious_python:main package --asset assets/python_companion.zip python_companion/ -p Linux --requirements "-r,python_companion/requirements.txt"
 
 # Strictly only use these packages for web compatibility:
 # https://pyodide.org/en/stable/usage/packages-in-pyodide.html
