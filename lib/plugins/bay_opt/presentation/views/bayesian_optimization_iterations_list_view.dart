7067e5cbaabe52e39a23715c9cd0195f27e2dd13
Sebastian <sebastian.franz@tum.de>
2025-09-30 12:32:26 +0200

    refactor: Adding bay_opt iteration result view that combines plotting and table view
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- lib/plugins/bay_opt/presentation/views/bayesian_optimization_iterations_list_view.dart
+++ lib/plugins/bay_opt/presentation/views/bayesian_optimization_iterations_list_view.dart
@@ -1,7 +1,5 @@
-import 'package:biocentral/plugins/prediction_models/bloc/biotrainer_training_bloc.dart';
-import 'package:biocentral/plugins/prediction_models/bloc/model_hub_bloc.dart';
-import 'package:biocentral/plugins/prediction_models/model/prediction_model.dart';
-import 'package:biocentral/plugins/prediction_models/presentation/displays/prediction_model_display.dart';
+import 'package:biocentral/plugins/bay_opt/bloc/bayesian_optimization_hub_bloc.dart';
+import 'package:biocentral/plugins/bay_opt/bloc/bayesian_optimization_iteration_bloc.dart';
 import 'package:biocentral/sdk/biocentral_sdk.dart';
 import 'package:biocentral/sdk/presentation/displays/biocentral_task_display.dart';
 import 'package:flutter/material.dart';
@@ -19,22 +17,54 @@ class _BayesianOptimizationIterationsListViewState extends State<BayesianOptimiz
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    final ModelHubBloc predictionModelsBloc = BlocProvider.of<ModelHubBloc>(context);
-    return BlocBuilder<ModelHubBloc, ModelHubState>(
-      builder: (context, state) {
-        return Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-              ],
-            ),
-          ),
+    return BlocBuilder<BayesianOptimizationHubBloc, BayesianOptimizationHubState>(
+      builder: (context, hubState) {
+        return BlocBuilder<BayesianOptimizationIterationBloc, BayesianOptimizationIterationState>(
+          builder: (context, iterationState) {
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    buildFinishedIterationsView(hubState),
+                  ],
+                ),
+              ),
+            );
+          },
         );
       },
     );
   }
 
+  Widget buildRunningIterationView(BayesianOptimizationIterationState iterationState) {
+    if (!iterationState.isOperating()) {
+      return Container();
+    }
+    // TODO [Refactor] Unify running task display widget with prediction models
+    return BiocentralTaskDisplay(
+      title: 'Running bo iteration..',
+      leadingIcon: const CircularProgressIndicator(),
+      trailing: BiocentralStatusIndicator(state: iterationState),
+      children: [],
+    );
+  }
+
+  Widget buildFinishedIterationsView(BayesianOptimizationHubState hubState) {
+    if (hubState.trainingResults.isEmpty) {
+      return const Text('No results yet!');
+    }
+    final taskDisplays = <Widget>[];
+    for (final (index, iterationResult) in hubState.trainingResults.indexed) {
+      taskDisplays.add(
+        BiocentralTaskDisplay(title: 'Iteration ${index + 1}', leadingIcon: const Icon(Icons.check), children: []),
+      );
+    }
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: taskDisplays,
+    );
+  }
 
   @override
   bool get wantKeepAlive => true;
