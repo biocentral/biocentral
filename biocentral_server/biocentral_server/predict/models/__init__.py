742dcd037eabc23ba6cb5f1df8836ea0d2355b5a
Sebastian <sebastian.franz@tum.de>
2025-11-11 12:31:46 +0100

    feat: Adding exotox predictor
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- biocentral_server/biocentral_server/predict/models/__init__.py
+++ biocentral_server/biocentral_server/predict/models/__init__.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List
 
 from .disorder import Seth
+from .toxicity import ExoTox
 from .binding import BindEmbed
 from .base_model import BaseModel
 from .variant_effect import VespaG
@@ -9,20 +10,25 @@ from .membrane import TMbed, LightAttentionMembrane
 from .secondary_structure import ProtT5SecondaryStructure
 from .localization import LightAttentionSubcellularLocalization
 
-
 MODEL_REGISTRY: Dict[str, Any] = {
-    TMbed.get_metadata().name: TMbed,
-    LightAttentionMembrane.get_metadata().name: LightAttentionMembrane,
-    LightAttentionSubcellularLocalization.get_metadata().name: LightAttentionSubcellularLocalization,
-    Seth.get_metadata().name: Seth,
-    BindEmbed.get_metadata().name: BindEmbed,
-    ProtT5Conservation.get_metadata().name: ProtT5Conservation,
-    ProtT5SecondaryStructure.get_metadata().name: ProtT5SecondaryStructure,
-    VespaG.get_metadata().name: VespaG,
+    name.lower(): model_class
+    for name, model_class in {
+        TMbed.get_metadata().name: TMbed,
+        LightAttentionMembrane.get_metadata().name: LightAttentionMembrane,
+        LightAttentionSubcellularLocalization.get_metadata().name: LightAttentionSubcellularLocalization,
+        Seth.get_metadata().name: Seth,
+        BindEmbed.get_metadata().name: BindEmbed,
+        ProtT5Conservation.get_metadata().name: ProtT5Conservation,
+        ProtT5SecondaryStructure.get_metadata().name: ProtT5SecondaryStructure,
+        VespaG.get_metadata().name: VespaG,
+        ExoTox.get_metadata().name: ExoTox,
+    }.items()
 }
 
 
 def filter_models(model_names: List[str]) -> Dict[str, Any]:
+    model_names = [model_name.lower() for model_name in model_names]
+
     assert all([model_name in MODEL_REGISTRY for model_name in model_names]), (
         "Invalid model name, this should have been caught in the endpoint"
     )
@@ -36,5 +42,6 @@ def filter_models(model_names: List[str]) -> Dict[str, Any]:
 
 def get_metadata_for_all_models() -> Dict[str, Any]:
     return {name: model.get_metadata() for name, model in MODEL_REGISTRY.items()}
+
 
 __all__ = ["filter_models", "get_metadata_for_all_models", "BaseModel"]
