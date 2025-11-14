c9542b0ca33269f0fddfed8fe5f103eec413694a
Sebastian <sebastian.franz@tum.de>
2025-11-14 14:52:33 +0100

    fix: Adding missing test files

---
+++ biocentral_api/dart/test/projection_request_test.dart
@@ -0,0 +1,35 @@
+import 'package:test/test.dart';
+import 'package:biocentral_api/biocentral_api.dart';
+
+// tests for ProjectionRequest
+void main() {
+  final instance = ProjectionRequestBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(ProjectionRequest, () {
+    // Sequence data to embed (seq_id -> sequence)
+    // BuiltMap<String, String> sequenceData
+    test('to test the property `sequenceData`', () async {
+      // TODO
+    });
+
+    // Projection method to use
+    // String method
+    test('to test the property `method`', () async {
+      // TODO
+    });
+
+    // Projection configuration
+    // BuiltMap<String, JsonObject> config
+    test('to test the property `config`', () async {
+      // TODO
+    });
+
+    // Name of the embedder model
+    // String embedderName
+    test('to test the property `embedderName`', () async {
+      // TODO
+    });
+
+  });
+}
