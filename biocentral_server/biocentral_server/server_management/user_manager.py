e8bfa275cff9c8dd236180b3c662e78189c3cca4
Sebastian <sebastian.franz@tum.de>
2026-03-16 12:01:56 +0100

    feat: Removing unused functions in user_manager
    Signed-off-by: Sebastian <sebastian.franz@tum.de>

--- biocentral_server/biocentral_server/server_management/user_manager.py
+++ biocentral_server/biocentral_server/server_management/user_manager.py
@@ -1,25 +1,10 @@
 import random
-import threading
 
 from hashlib import md5
-from typing import Dict
 from fastapi import Request
-
-_user_dict: Dict = {"n_total_requests": 0}
-_lock = threading.Lock()
 
 
 class UserManager:
-    @staticmethod
-    def check_request(req: Request):
-        with _lock:
-            _user_dict["n_total_requests"] += 1
-
-    @staticmethod
-    def get_total_number_of_requests_since_start() -> int:
-        with _lock:
-            return _user_dict["n_total_requests"]
-
     @staticmethod
     async def get_random_user_id(req: Request) -> str:
         """Must only be used to disable rate limiting for debug mode"""
